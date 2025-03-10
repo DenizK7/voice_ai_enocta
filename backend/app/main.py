1466a8e171693a9906e44dc8fbee51e2941eb67c
DenizK7 <deniz.kucukkara@ceng.deu.edu.tr>
2025-03-10 10:53:02 +0300

    a

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,4 +4,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='https://front-production-5a59.up.railway.app/', port=8080)
